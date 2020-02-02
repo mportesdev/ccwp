2454b7007b3d677aa90707937536867ee046a070
myrmica-habilis <michalportes1@gmail.com>
2020-02-02 13:49:01 +0100

    Refactor names

---
+++ transposition_encrypt.py
@@ -0,0 +1,30 @@
+# https://www.nostarch.com/crackingcodes/ (BSD Licensed)
+
+import pyperclip
+
+
+def main():
+    my_message = 'Common sense is not so common.'
+    my_key = 8
+
+    cipher_text = encrypt_message(my_key, my_message)
+
+    print(cipher_text + '|')
+    pyperclip.copy(cipher_text)
+
+
+def encrypt_message(key, message):
+    cipher_columns = [''] * key
+
+    for column in range(key):
+        current_index = column
+
+        while current_index < len(message):
+            cipher_columns[column] += message[current_index]
+            current_index += key
+
+    return ''.join(cipher_columns)
+
+
+if __name__ == '__main__':
+    main()
