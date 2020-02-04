41c24613194ce135d1703d331696086c860b7a04
myrmica-habilis <michalportes1@gmail.com>
2020-02-04 17:27:09 +0100

    Refactor names

---
+++ transposition_decrypt.py
@@ -0,0 +1,39 @@
+# https://www.nostarch.com/crackingcodes/ (BSD Licensed)
+
+import math, pyperclip
+
+
+def main():
+    my_message = 'Cenoonommstmme oo snnio. s s c'
+    my_key = 8
+
+    plaintext = decrypt_message(my_key, my_message)
+
+    print(plaintext + '|')
+    pyperclip.copy(plaintext)
+
+
+def decrypt_message(key, message):
+    num_of_columns = math.ceil(len(message) / key)
+    num_of_rows = key
+    shaded_boxes = num_of_columns * num_of_rows - len(message)
+
+    plaintext = [''] * num_of_columns
+
+    column = 0
+    row = 0
+
+    for symbol in message:
+        plaintext[column] += symbol
+        column += 1
+
+        if column == num_of_columns or (column == num_of_columns - 1 and
+                                        row >= num_of_rows - shaded_boxes):
+            column = 0
+            row += 1
+
+    return ''.join(plaintext)
+
+
+if __name__ == '__main__':
+    main()
