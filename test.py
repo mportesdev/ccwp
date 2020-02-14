2640667633a96a631205f941a0edc10eb53747c0
myrmica-habilis <michalportes1@gmail.com>
2020-02-14 10:55:33 +0100

    Parametrize key

--- test.py
+++ test.py
@@ -8,9 +8,10 @@ from transposition_file_cipher import translate_file
 @pytest.mark.parametrize('name', ('frankenstein',
                                   'romeo_and_juliet',
                                   'the_time_machine'))
-def test_transposition_file_cipher(name):
-    translate_file(f'{name}.txt', 10, 'encrypt')
-    translate_file(f'{name}_encrypted.txt', 10, 'decrypt')
+@pytest.mark.parametrize('key', (10, 645, 3507, 69528))
+def test_transposition_file_cipher(name, key):
+    translate_file(f'{name}.txt', key, 'encrypt')
+    translate_file(f'{name}_encrypted.txt', key, 'decrypt')
 
     with open(f'{name}.txt') as original_file, \
             open(f'{name}_encrypted_decrypted.txt') as roundtrip_file:
