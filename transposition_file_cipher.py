c81a0ef3d2da67a5d0e5c8dbbb0972359845a90a
myrmica-habilis <michalportes1@gmail.com>
2020-02-14 10:17:28 +0100

    Remove system exit, raise exceptions

--- transposition_file_cipher.py
+++ transposition_file_cipher.py
@@ -1,7 +1,6 @@
 # https://www.nostarch.com/crackingcodes/ (BSD Licensed)
 
 import os
-import sys
 import time
 
 import transposition_decrypt
@@ -10,8 +9,7 @@ import transposition_encrypt
 
 def main(input_filename, my_key, my_mode):
     if not os.path.exists(input_filename):
-        print(f'The file {input_filename!r} does not exist. Quitting...')
-        sys.exit()
+        raise OSError(f'The file {input_filename!r} does not exist.')
 
     with open(input_filename) as input_file:
         content = input_file.read()
@@ -23,6 +21,9 @@ def main(input_filename, my_key, my_mode):
         translated = transposition_encrypt.encrypt_message(my_key, content)
     elif my_mode == 'decrypt':
         translated = transposition_decrypt.decrypt_message(my_key, content)
+    else:
+        raise ValueError(f"Unexpected my_mode: {my_mode!r}"
+                         f" (must be 'encrypt' or 'decrypt')")
     total_time = round(time.time() - start_time, 2)
     print(f'{my_mode.title()}ion time: {total_time} seconds')
 
