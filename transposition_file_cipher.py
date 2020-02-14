0c28ab29942f86a279586f39c6d8d67b29711c1e
myrmica-habilis <michalportes1@gmail.com>
2020-02-14 10:19:28 +0100

    Refactor names

--- transposition_file_cipher.py
+++ transposition_file_cipher.py
@@ -7,34 +7,34 @@ import transposition_decrypt
 import transposition_encrypt
 
 
-def main(input_filename, my_key, my_mode):
+def main(input_filename, key, mode):
     if not os.path.exists(input_filename):
         raise OSError(f'The file {input_filename!r} does not exist.')
 
     with open(input_filename) as input_file:
         content = input_file.read()
 
-    print(f'{my_mode.title()}ing...')
+    print(f'{mode.title()}ing...')
 
     start_time = time.time()
-    if my_mode == 'encrypt':
-        translated = transposition_encrypt.encrypt_message(my_key, content)
-    elif my_mode == 'decrypt':
-        translated = transposition_decrypt.decrypt_message(my_key, content)
+    if mode == 'encrypt':
+        translated = transposition_encrypt.encrypt_message(key, content)
+    elif mode == 'decrypt':
+        translated = transposition_decrypt.decrypt_message(key, content)
     else:
-        raise ValueError(f"Unexpected my_mode: {my_mode!r}"
+        raise ValueError(f"Unexpected mode: {mode!r}"
                          f" (must be 'encrypt' or 'decrypt')")
     total_time = round(time.time() - start_time, 2)
-    print(f'{my_mode.title()}ion time: {total_time} seconds')
+    print(f'{mode.title()}ion time: {total_time} seconds')
 
     stem, suffix = os.path.splitext(input_filename)
-    output_filename = f'{stem}_{my_mode}ed{suffix}'
+    output_filename = f'{stem}_{mode}ed{suffix}'
 
     with open(output_filename, 'w') as output_file:
         output_file.write(translated)
 
-    print(f'Done {my_mode}ing {input_filename!r} ({len(content)} characters).')
-    print(f'{my_mode.title()}ed file is {output_filename!r}.')
+    print(f'Done {mode}ing {input_filename!r} ({len(content)} characters).')
+    print(f'{mode.title()}ed file is {output_filename!r}.')
 
 
 if __name__ == '__main__':
