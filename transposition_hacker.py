bfdce61b362c5d48392a0147aca9c4081c3cfa05
myrmica-habilis <michalportes1@gmail.com>
2020-02-14 11:16:20 +0100

    Add transposition_hacker.py from Chapter 12

---
+++ transposition_hacker.py
@@ -0,0 +1,52 @@
+# https://www.nostarch.com/crackingcodes/ (BSD Licensed)
+
+import pyperclip
+
+import detect_english
+import transposition_decrypt
+
+
+def main():
+    my_message = ("AaKoosoeDe5 b5sn ma reno ora'lhlrrceey e  enlh na  indeit n"
+                  " uhoretrm au ieu v er Ne2 gmanw,forwnlbsya apor tE.no"
+                  " euarisfatt  e mealefedhsppmgAnlnoe(c -or)alat r lw o eb"
+                  "  nglom,Ain one dtes ilhetcdba. t tg eturmudg,tfl1e1 v"
+                  "  nitiaicynhrCsaemie-sp ncgHt nie cetrgmnoa yc r,ieaa"
+                  "  toesa- e a0m82e1w shcnth  ekh gaecnpeutaaieetgn iodhso d"
+                  " ro hAe snrsfcegrt NCsLc b17m8aEheideikfr aBercaeu"
+                  " thllnrshicwsg etriebruaisss  d iorr.")
+
+    hacked_message = hack_transposition(my_message)
+
+    if hacked_message is None:
+        print('Failed to hack encryption.')
+    else:
+        print('Copying hacked message to clipboard:')
+        print(hacked_message)
+        pyperclip.copy(hacked_message)
+
+
+def hack_transposition(message):
+    print('Hacking...')
+    print('(Press Ctrl-C (on Windows) or Ctrl-D (on macOS and Linux)'
+          ' to quit at any time.)')
+
+    for key in range(1, len(message)):
+        print(f'Trying key #{key}...')
+
+        decrypted_text = transposition_decrypt.decrypt_message(key, message)
+
+        if detect_english.is_english(decrypted_text):
+            print()
+            print('Possible encryption hack:')
+            print(f'Key {key}: {decrypted_text[:100]}')
+            print()
+            print('Enter D if done, anything else to continue hacking:')
+            response = input('> ')
+
+            if response.strip().upper().startswith('D'):
+                return decrypted_text
+
+
+if __name__ == '__main__':
+    main()
