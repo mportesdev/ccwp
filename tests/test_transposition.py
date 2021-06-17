301905386b6a6ca6a8aa233dd9c1a1a4efff4441
Michal Porteš <michalportes1@gmail.com>
2021-06-17 10:54:57 +0200

    Remove file translation module

--- tests/test_transposition.py
+++ tests/test_transposition.py
@@ -1,10 +1,8 @@
-import os
 from pathlib import Path
 
 import pytest
 
 from transposition import encrypt, decrypt
-from transposition_file_cipher import translate_file
 
 
 @pytest.mark.parametrize(
@@ -37,15 +35,10 @@ def test_decrypt(message, key, expected):
     assert decrypt(key, message) == expected
 
 
-@pytest.mark.parametrize('name', ('frankenstein', 'the_time_machine'))
+@pytest.mark.parametrize('filename', ('frankenstein.txt',
+                                      'the_time_machine.txt'))
 @pytest.mark.parametrize('key', (10, 645, 3507))
-def test_transposition_file_cipher(name, key):
-    translate_file(f'{name}.txt', key, 'encrypt')
-    translate_file(f'{name}_encrypted.txt', key, 'decrypt')
-
-    original_file = Path(f'{name}.txt')
-    roundtrip_file = Path(f'{name}_encrypted_decrypted.txt')
-    assert original_file.read_text() == roundtrip_file.read_text()
-
-    os.remove(f'{name}_encrypted.txt')
-    os.remove(f'{name}_encrypted_decrypted.txt')
+def test_file_roundtrip(filename, key):
+    original = Path(filename).read_text()
+    round_trip = decrypt(key, encrypt(key, original))
+    assert original == round_trip
