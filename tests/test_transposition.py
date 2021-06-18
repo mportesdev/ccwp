617e3d47c5aff7efd68ee39ae763daf9d01ab788
Michal Porteš <michalportes1@gmail.com>
2021-06-18 10:19:37 +0200

    Refactor transposition hacker, add unit test

--- tests/test_transposition.py
+++ tests/test_transposition.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import pytest
 
 from transposition import encrypt, decrypt
+from transposition_hacker import hack
 
 
 @pytest.mark.parametrize(
@@ -42,3 +43,27 @@ def test_file_roundtrip(filename, key):
     original = Path(filename).read_text()
     round_trip = decrypt(key, encrypt(key, original))
     assert original == round_trip
+
+
+CIPHER_TEXT = ("AaKoosoeDe5 b5sn ma reno ora'lhlrrceey e  enlh na  indeit n"
+               " uhoretrm au ieu v er Ne2 gmanw,forwnlbsya apor tE.no"
+               " euarisfatt  e mealefedhsppmgAnlnoe(c -or)alat r lw o eb"
+               "  nglom,Ain one dtes ilhetcdba. t tg eturmudg,tfl1e1 v"
+               "  nitiaicynhrCsaemie-sp ncgHt nie cetrgmnoa yc r,ieaa"
+               "  toesa- e a0m82e1w shcnth  ekh gaecnpeutaaieetgn iodhso d"
+               " ro hAe snrsfcegrt NCsLc b17m8aEheideikfr aBercaeu"
+               " thllnrshicwsg etriebruaisss  d iorr.")
+
+PLAIN_TEXT = ("Augusta Ada King-Noel, Countess of Lovelace (10 December 1815"
+              " - 27 November 1852) was an English mathematician and writer,"
+              " chiefly known for her work on Charles Babbage's early"
+              " mechanical general-purpose computer, the Analytical Engine."
+              " Her notes on the engine include what is recognised as the first"
+              " algorithm intended to be carried out by a machine. As a result,"
+              " she is often regarded as the first computer programmer.")
+
+
+def test_hack():
+    plain_text, key = hack(CIPHER_TEXT)
+    assert plain_text == PLAIN_TEXT
+    assert key == 6
--- transposition_hacker.py
+++ transposition_hacker.py
@@ -1,48 +1,10 @@
-# https://www.nostarch.com/crackingcodes/ (BSD Licensed)
-
-import detect_english
+from detect_english import is_english
 from transposition import decrypt
 
 
-def main():
-    my_message = ("AaKoosoeDe5 b5sn ma reno ora'lhlrrceey e  enlh na  indeit n"
-                  " uhoretrm au ieu v er Ne2 gmanw,forwnlbsya apor tE.no"
-                  " euarisfatt  e mealefedhsppmgAnlnoe(c -or)alat r lw o eb"
-                  "  nglom,Ain one dtes ilhetcdba. t tg eturmudg,tfl1e1 v"
-                  "  nitiaicynhrCsaemie-sp ncgHt nie cetrgmnoa yc r,ieaa"
-                  "  toesa- e a0m82e1w shcnth  ekh gaecnpeutaaieetgn iodhso d"
-                  " ro hAe snrsfcegrt NCsLc b17m8aEheideikfr aBercaeu"
-                  " thllnrshicwsg etriebruaisss  d iorr.")
-
-    hacked_message = hack_transposition(my_message)
-
-    if hacked_message is None:
-        print('Failed to hack encryption.')
-    else:
-        print(hacked_message)
-
-
-def hack_transposition(message):
-    print('Hacking...')
-    print('(Press Ctrl-C (on Windows) or Ctrl-D (on macOS and Linux)'
-          ' to quit at any time.)')
-
+def hack(message):
     for key in range(1, len(message)):
-        print(f'Trying key #{key}...')
-
         decrypted_text = decrypt(key, message)
 
-        if detect_english.is_english(decrypted_text):
-            print()
-            print('Possible encryption hack:')
-            print(f'Key {key}: {decrypted_text[:100]}')
-            print()
-            print('Enter D if done, anything else to continue hacking:')
-            response = input('> ')
-
-            if response.strip().upper().startswith('D'):
-                return decrypted_text
-
-
-if __name__ == '__main__':
-    main()
+        if is_english(decrypted_text):
+            return decrypted_text, key
