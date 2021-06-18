4e0c2f056d63668166aade0878da47b2142b48b1
Michal Porteš <michalportes1@gmail.com>
2021-06-18 11:37:22 +0200

    Add public key cipher module

---
+++ public_key.py
@@ -0,0 +1,26 @@
+import random
+import math
+
+from cryptomath import find_mod_inverse
+from primes import generate_large_prime
+
+
+def generate_keys(key_size):
+    while True:
+        p = generate_large_prime(key_size)
+        q = generate_large_prime(key_size)
+        if p != q:
+            break
+
+    while True:
+        e = random.randrange(2 ** (key_size - 1), 2 ** key_size)
+        if math.gcd(e, (p - 1) * (q - 1)) == 1:
+            break
+
+    n = p * q
+    public_key = n, e
+
+    d = find_mod_inverse(e, (p - 1) * (q - 1))
+    private_key = n, d
+
+    return public_key, private_key
